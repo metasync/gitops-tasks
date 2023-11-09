d1b398673a15ef2af73b9812332322d6d8ac121e
Chi Man Lei <chimanlei@gmail.com>
2023-11-09 09:00:05 +0800

    Fixed typos on file path

--- pipelines/setup.sh
+++ pipelines/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # GitOps bootstrap
-kubectl apply -f gitops_boostrap/gitops_repo_pipeline.yaml
-kubectl apply -f gitops_boostrap/quay_repo_pipeline.yaml
-kubectl apply -f gitops_boostrap/kube_app_pipeline.yaml
-kubectl apply -f gitops_boostrap/argocd_app_pipeline.yaml
+kubectl apply -f gitops_bootstrap/gitops_repo_pipeline.yaml
+kubectl apply -f gitops_bootstrap/quay_repo_pipeline.yaml
+kubectl apply -f gitops_bootstrap/kube_app_pipeline.yaml
+kubectl apply -f gitops_bootstrap/argocd_app_pipeline.yaml
 
 # GitOps operations
 kubectl apply -f gitops_operations/quay_image_check_updates_pipeline.yaml
