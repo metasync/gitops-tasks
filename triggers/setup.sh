25fa8e29ac5cfdbaecda6209875602b5e4d7326e
Chi Man Lei <chimanlei@gmail.com>
2023-11-16 23:10:43 +0800

    added event listener ingress

--- triggers/setup.sh
+++ triggers/setup.sh
@@ -19,3 +19,4 @@ kubectl apply -f quay_image_rebuild_trigger.yaml
 kubectl apply -f quay_image_rebuild_trigger_template.yaml
 
 kubectl apply -f tekton_ci_event_listener.yaml
+kubectl apply -f tekton_ci_event_listener_ingress.yaml
